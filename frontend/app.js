c937f8438968e48d33a723735ba538ae05f86fa8
Nicklodeon <54819133+Nicklodeon@users.noreply.github.com>
2026-08-13 23:07:19 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,8 +1,8 @@
 const SUPABASE_URL =
-    "YOUR_SUPABASE_URL";
+    "https://gmytscoqupsozionnryy.supabase.co";
 
 const SUPABASE_KEY =
-    "YOUR_SUPABASE_PUBLISHABLE_KEY";
+    "sb_publishable_dpY7xVY8df2CqDfoT9rTFg_PGpgpWNF";
 
 const { createClient } =
     supabase;
